309211dfecac9fb7142db501c6a52faee29416f9
kjayaprakash <kjayaprakash4028@gmail.com>
2018-08-05 11:59:13 -0400

    mybatis code changes

--- src/main/java/com/jp/userregistration/dao/UserRegistrationDAO.java
+++
@@ -1,7 +0,0 @@
-package com.jp.userregistration.dao;
-
-import com.jp.couch.user.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRegistrationDAO extends CrudRepository<User,String> {
-}
